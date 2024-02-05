989774719c78aa5bc9ccf3addf86db606ad27f99
User <nawodilharaamb@gmail.com>
2024-02-05 20:53:39 +0530

    Add project files.

---
+++ MvcLogin/Controllers/AccountController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcLogin.Models;
+
+namespace MvcLogin.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            using (OurDbContext db = new OurDbContext())
+            {
+                return View(db.userAccounts.ToList());
+
+            }
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+                using (OurDbContext db = new OurDbContext())
+                {
+                    db.userAccounts.Add(account);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = account.FirstName + "" + account.LastName + "successfully registered.";
+
+            }
+            return View();
+        }
+
+        //Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(UserAccount user)
+        {
+            using (OurDbContext db = new OurDbContext())
+            {
+                var usr = db.userAccounts.Single(u => u.UserName == user.UserName && u.Password == user.Password);
+                if (usr != null)
+                {
+                    Session["UserID"] = usr.UserID.ToString();
+                    Session["username"] = usr.UserName.ToString();
+                    return RedirectToAction("LoggedIn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or Passwoed is wrong.");
+
+                }
+            }
+            return View();
+        
+        
+        
+        }
+        public ActionResult LoggedIn()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+    }
+}
